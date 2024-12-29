1e3800e555a524fcc7bc15d04aea5d5f67cd42d5
Hasnain Narsandawala <hasnain.narsandawala@gmail.com>
2024-12-29 17:15:30 +0530

    File compresson program has been done

--- gui.py
+++ gui.py
@@ -8,16 +8,19 @@ add_button = sg.Button("Add")
 list_box = sg.Listbox(values=functions.get_todos(), key='todos',
                       enable_events=True, size=[45,10])
 edit_button = sg.Button("Edit")
+exit_button  = sg.Button("Exit")
+
+
 
 window = sg.Window('My To-Do App', 
-                   layout=[[label],[input_box,add_button],[list_box,edit_button]], 
+                   layout=[[label],[input_box,add_button],[list_box,edit_button], [exit_button]], 
                    font=('Helvetica',15))
 
 while True:
     event,values = window.read()
-    print(1,event)
-    print(2,values)
-    print(3,values['todos'])
+    # print(1,event)
+    # print(2,values)
+    # print(3,values['todos'])
     match event:
         case "Add":
             todos = functions.get_todos()
@@ -38,6 +41,12 @@ while True:
 
         case 'todos':
             window['todo'].update(value=values['todos'][0])
+        
+        case 'Exit':
+            break
+            
         case sg.WIN_CLOSED:
             break
+
 window.close()
+
