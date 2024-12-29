92a86591e6a8cad204b146228030a250ee0b6a66
Hasnain Narsandawala <hasnain.narsandawala@gmail.com>
2024-12-29 14:41:34 +0530

    GUI updated

--- functions.py
+++ functions.py
@@ -1,10 +1,10 @@
-FILEPATH = "todos_items.txt"
+FILEPATH = "todos.txt"
 
 def get_todos(filepath = FILEPATH):
     """ Read a text file and return the list of
     to-do items"""
     with open(filepath, 'r') as file_local:
-        todos_local = filepath.readlines()
+        todos_local = file_local.readlines()
     return todos_local
 
 
